d4330652a63a733f38acb855cd5c0e3e9981448c
MaryMisMaria <mariia.mys.mpmpm.2021@lpnu.ua>
2024-12-16 12:10:32 +0200

    add-feature

--- src/components/CityDetails.tsx
+++ src/components/CityDetails.tsx
@@ -1,15 +1,21 @@
 import React, { FC } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-const CityDetails: FC = () => {
+const CityDetailsPage: FC = () => {
+  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
+
+  const handleBackClick = () => {
+    navigate('/');
+  };
 
   return (
     <div>
-      <p>City ID: {id}</p>
+      <button onClick={handleBackClick}>Back to Home</button>
       <h1>City Details</h1>
+      <p>City ID: {id}</p>
     </div>
   );
 };
 
-export default CityDetails;
+export default CityDetailsPage;
